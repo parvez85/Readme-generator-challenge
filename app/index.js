09319be7de3179bbc2da87e391d1acaaeb12bab7
AP <a.parvez85@gmail.com>
2023-05-16 20:24:16 +0100

    adding index.jsand generate markdown file

--- app/index.js
+++ app/index.js
@@ -1,21 +1,97 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// array of the questions for users
+const questions = [ 
+
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title',
+        default: 'Project Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project title is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Write a description of your project.",
+        name: 'description',
+        default: 'Project Description',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid project description is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: " Describe the steps required to install your project for the Installation section.",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "Provide instructions and examples of your project in use",
+        name: 'usage'
+    },
+    
+    {
+        type: 'input',
+        message: "What test were ran when creating this project.",
+        name: 'tests'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: [ 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, data
+    ) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Success! Your README.md file has been generated")
+    });
 }
 
-// function to initialize program
-function init() {
+const writeFileAsync = util.promisify(writeToFile);
 
-}
 
-// function call to initialize program
+// Main function
+async function init() {
+    try {
+
+        // Prompt Inquirer questions
+        const userResponses = await inquirer.prompt(questions);
+        console.log("Your responses: ", userResponses);
+        console.log("Thank you for your responses! Fetching your GitHub data next..."); 
+
+        // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
+        console.log("Generating your README next...")
+        const markdown = generateMarkdown(userResponses);
+        console.log(markdown);
+    
+        // Write markdown to file
+        await writeFileAsync('README.md', markdown);
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 init();
--- app/utils/generateMarkdown.js
+++ app/utils/generateMarkdown.js
@@ -1,8 +1,115 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(userResponses) {
 
-`;
+  // Generate Table of Contents conditionally based on userResponses
+  let draftToC = `## Table of Contents`;
+
+  if (userResponses.installation !== '') { draftToC += `
+  * [Installation](#installation)` };
+
+  if (userResponses.usage !== '') { draftToC += `
+  * [Usage](#usage)` };
+
+  if (userResponses.contributing !== '') { draftToC += `
+  * [Contributing](#contributing)` };
+
+  if (userResponses.tests !== '') { draftToC += `
+  * [Tests](#tests)` };
+
+
+  // Generate markdown for the top required portions of the README
+  let draftMarkdown = 
+  `# ${userResponses.title}
+  
+  
+  ## Description
+  
+ 
+  
+  ${userResponses.description}
+
+  `
+
+  // Add Table of Contents to markdown
+  draftMarkdown += draftToC;
+ 
+  // Add License section since License is required to Table of Contents
+  draftMarkdown += `
+  * [License](#license)`;
+  
+
+  // Optional Installation section
+  if (userResponses.installation !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Installation
+  
+  *Steps required to install project and how to get the development environment running:*
+  
+  ${userResponses.installation}`
+  };
+  
+
+  // Optional Usage section
+  if (userResponses.usage !== '') {
+  
+  draftMarkdown +=
+  
+  `
+  
+  ## Usage 
+  
+  *Instructions and examples for use:*
+  
+  ${userResponses.usage}`
+  };
+  
+  
+  // Optional Contributing section
+  if (userResponses.contributing !== '') {
+
+  draftMarkdown +=
+    
+  `
+  
+  ## Contributing
+  
+  *If you would like to contribute it, you can follow these guidelines for how to do so.*
+  
+  ${userResponses.contributing}`
+  };
+  
+
+  // Optional Tests section
+  if (userResponses.tests !== '') {
+  
+  draftMarkdown +=
+  `
+  
+  ## Tests
+  
+  *Tests for application and how to run them:*
+  
+  ${userResponses.tests}`
+  };
+
+
+  // License section is required
+  draftMarkdown +=
+  `
+  
+  ## License
+  
+  ${userResponses.license}
+  `;
+
+
+  
+  // Return markdown
+  return draftMarkdown;
+  
 }
 
 module.exports = generateMarkdown;
+
